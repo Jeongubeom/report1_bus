654bf09c579d890fa1221caedfb42d4303cb6e5e
ubeomJeong <owl1225@naver.com>
2022-11-25 15:42:54 +0900

    속도 추가

--- src/Bus.java
+++ src/Bus.java
@@ -16,6 +16,7 @@ public class Bus {
     public Bus(int bNum) {
         //busNUM 생성할때
         this.busNum=bNum;
+        presentSpeed = 0;
         maxPassenger = 30; //최대승객 30;
         cost = 1000; //버스요금 2000;
         oil = 100; //주유량 초기값0
@@ -24,7 +25,7 @@ public class Bus {
 
     int getPresentPassenger(int presentPassenger) { //현재 승객 n명 타면 남은 승객 반환
         int peple = maxPassenger - presentPassenger;
-        int onCost = presentPassenger * 1000;
+        int onCost = presentPassenger * 1000; //1당 요금 1000씩 부과
         System.out.println("현재 탑승승객 수 : "+presentPassenger);
         System.out.println("잔여 승객 수 : "+peple);
         System.out.println("요금 확인 : "+onCost);
@@ -34,7 +35,7 @@ public class Bus {
     int getOilUse(int useOil){ //기름 사용 량
          oil -= useOil;
         System.out.println("주유량 : "+oil);
-         if(oil < 10){
+         if(oil < 10){ //10미만일시 얼랏
              System.out.println("차고지로 향합니다");
              System.out.println("주유가 필요합니다");
          }
@@ -62,7 +63,15 @@ public class Bus {
         return presentPassenger;
     }
 
-
+    int upSpeed(int upSpeed){ //속도 증가
+        presentSpeed += upSpeed;
+        return presentSpeed;
+    }
+    int downSpeed(int downSpeed){ //속도 감소
+        presentSpeed -=downSpeed;
+        return presentSpeed;
+    }
+    
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Bus bus = new Bus(5);
+        Bus bus = new Bus(2);
         bus.getPresentPassenger(5);
         bus.getOilUse(50);
         bus.isRunOrGarage(true);
